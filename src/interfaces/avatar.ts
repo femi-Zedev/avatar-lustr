9a2a64a134485a68fcccdff209e120f799fa85de
heyolaniran <olaniran.abd@gmail.com>
2024-02-05 16:40:08 +0100

    structure of an avatar for reseach

--- src/interfaces/avatar.ts
+++ src/interfaces/avatar.ts
@@ -2,7 +2,15 @@ export interface AvatarProps {
   imgUrl: string;
   name: string;
   link: string;
-  sexe: 'male' | 'female';
-  race: 'caucasien' | 'oceanien' | 'afro' | 'asiat' | 'amerindien';
+  sexe: 'male' | 'female' | '*';
+  race: 'caucasien' | 'oceanien' | 'afro' | 'asiat' | 'amerindien' | "*";
   author: string
+}
+
+export interface Gender {
+  
+    male: 'male', 
+    female: 'female',
+    both: '*'
+   
 }
